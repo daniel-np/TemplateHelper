ee49b54bb6a1884f7ac1c75364c7900986eadcda
Daniel pettersen <d.pettersen@gmail.com>
2020-03-09 12:33:51 +0000

    Replaced start, stop with length and arrayList with indexes

--- src/main/java/model/TemplateTextField.java
+++ src/main/java/model/TemplateTextField.java
@@ -1,33 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+
 public class TemplateTextField {
-    private int start, stop;
+    private int length;
+    private ArrayList<Integer> locations;
     private String templateTextField;
 
-    public TemplateTextField(int start, int stop, String templateTextField) {
-        this.start = start;
-        this.stop = stop;
+    public TemplateTextField(int length, String templateTextField) {
+        this.length = length;
         this.templateTextField = templateTextField;
+        this.locations = new ArrayList<>();
     }
 
     public TemplateTextField() {
 
     }
 
-    public int getStart() {
-        return start;
+    public int getLength() {
+        return length;
     }
 
-    public void setStart(int start) {
-        this.start = start;
+    public void setLength(int length) {
+        this.length = length;
     }
 
-    public int getStop() {
-        return stop;
+    public ArrayList<Integer> getLocations(){
+        return locations;
     }
 
-    public void setStop(int stop) {
-        this.stop = stop;
+    public void addLocation(int loc) {
+        locations.add(loc);
     }
 
     public String getTemplateTextField() {
@@ -35,11 +38,19 @@ public class TemplateTextField {
     }
 
     public void setTemplateTextField(String templateTextField) {
+        if(this.templateTextField.length() != templateTextField.length()) {
+            this.length = templateTextField.length();
+        }
         this.templateTextField = templateTextField;
     }
 
     @Override
     public String toString() {
-        return templateTextField.substring(2,templateTextField.length()-2);
+        StringBuilder sb = new StringBuilder();
+        for(int i : locations) {
+            sb.append(i).append(", ");
+        }
+        return templateTextField.substring(2,templateTextField.length()-2) +
+                ": " + sb.toString().substring(0, sb.toString().length()-2);
     }
 }
